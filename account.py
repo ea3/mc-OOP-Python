eb6c4d248bf37a3de7cf5236c9c2dc898efa3c2d
Emilio Araos <ea3dev@gmail.com>
2020-02-04 00:05:38 -0700

    added song class

--- account.py
+++ account.py
@@ -6,35 +6,36 @@ class Account:
     """Simple account class with balance"""
 
     @staticmethod
-    def _current_time():      # _method, are non public. non intended to be used outside of the class. STATIC METHOD, USE THE CLASS TO ACCESS THEM 
+    def _current_time():      # _method, are non public. non intended to be used outside class.
         utc_time = datetime.datetime.utcnow()
         return pytz.utc.localize(utc_time)
 
     def __init__(self, name, balance):
-        self.name = name
-        self.balance = balance
-        self.transaction_list = []
-        print("Account created for " + self.name)
+        self._name = name           # Attributes starting with underscore are not meant to use outside of the class.
+        self.__balance = balance    # __ intended to be used in subclassing.
+        self._transaction_list = [(Account._current_time(), balance)]
+        print("Account created for " + self._name)
+        self.show_balance()
 
     def deposit(self, amount):
         if amount > 0:
-            self.balance += amount
+            self.__balance += amount
             self.show_balance()
-            self.transaction_list.append((Account._current_time(), amount))
+            self._transaction_list.append((Account._current_time(), amount))
 
     def withdraw(self, amount):
-        if 0 < amount <= self.balance:
-            self.balance -= amount
-            self.transaction_list.append((Account._current_time(), -amount))
+        if 0 < amount <= self.__balance:
+            self.__balance -= amount
+            self._transaction_list.append((Account._current_time(), -amount))
         else:
             print("Sorry. You are broke. Not enough money on the bank")
             self.show_balance()
 
     def show_balance(self):
-        print("Balance is {}".format(self.balance))
+        print("Balance is {}".format(self.__balance))
 
     def show_transactions(self):
-        for date, amount in self.transaction_list:
+        for date, amount in self._transaction_list:
             if amount > 0:
                 transaction_type = "Deposited"
             else:
@@ -47,11 +48,21 @@ if __name__ == '__main__':
     Emilio = Account("Emilio", 0)
     Emilio.show_balance()
 
-    Emilio.deposit(1000000)
-    Emilio.deposit(1000000)
-    Emilio.withdraw(500000)
+    Emilio.deposit(1000)
+    Emilio.withdraw(500)
+
+    Emilio.withdraw(200)
 
     Emilio.show_transactions()
+
+    Lucia = Account("Lucia", 800)
+    Lucia.__balance = 200
+    Lucia.deposit(100)
+    Lucia.withdraw(200)
+    Lucia.show_transactions()
+    print(Lucia.__dict__)
+
+
 
 
 
---
+++ songs.py
@@ -0,0 +1,23 @@
+class Song:
+    """Class to represent  a song
+
+    Attributes:
+    title (str): The title of the song
+    artist(Artist): An artist object representing the song creator.
+    duration (int): The duration of the song in seconds. May be zero
+    """
+    def __init__(self, title, artist, duration=0):
+        """ Song init method
+        Args:
+            title (str): Initialises the 'title' attribute
+            artist (Artist): At Artist object representing the song's creator
+            duration(Optional[int]: Initial value of 'duration' attribute
+            Will default to zero if not specified)
+        """
+
+        self.title = title
+        self.artist = artist
+        self.duration = duration
+        
+
+
