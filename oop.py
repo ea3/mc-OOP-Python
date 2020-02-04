7c767362641d5f2accd0394589af02383b75a0ee
Emilio Araos <ea3dev@gmail.com>
2020-02-03 21:44:49 -0700

    introducing class attributes

--- oop.py
+++ oop.py
@@ -1,5 +1,7 @@
 class Kettle(object):
 
+    power_source = "electrocity"       # THIS IS A CLASS ATTRIBUTE. ALL INSTANCE  VARIABLES WILL SHARE THIS ATTRIBUTE.
+
     def __init__(self, make, price):
         self.make = make
         self.price = price
@@ -17,6 +19,9 @@ kenwood.price = 12.75
 print(kenwood.price)
 
 hamilton = Kettle("Hamilton", 14.55)
+hamilton.switch_on()
+print(hamilton.on)
+
 print(hamilton.make)
 print(hamilton.price)
 
@@ -34,4 +39,26 @@ Attribute: a variable bound to an instance of a class.
 print(hamilton.on)
 hamilton.switch_on()
 print(hamilton.on)
+
+Kettle.switch_on(kenwood)
+print(kenwood.on)
+
+print("********")
+kenwood.power = 1.5
+print(kenwood.power)
+hamilton.power = 5
+print(hamilton.power)
+Kettle.power_source = "Atomic power"
+kenwood.power_source = "Un cacharro viejo"
+
+# Subclasses are preferable for adding functionality to instances.
+
+print(Kettle.power_source)
+print(kenwood.power_source)
+print(hamilton.power_source)
+
+print(Kettle.__dict__)
+print(kenwood.__dict__)
+print(hamilton.__dict__)
+
 
