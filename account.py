3c2bb43a0aa580a4edc279593ecb10b6b9f41015
Emilio Araos <ea3dev@gmail.com>
2020-02-03 23:13:31 -0700

    importing datetime and pytz to record transaction time

--- account.py
+++ account.py
@@ -1,15 +1,21 @@
+import datetime
+import pytz
+
+
 class Account:
     """Simple account class with balance"""
 
     def __init__(self, name, balance):
         self.name = name
         self.balance = balance
+        self.transaction_list = []
         print("Account created for " + self.name)
 
     def deposit(self, amount):
         if amount > 0:
             self.balance += amount
             self.show_balance()
+            self.transaction_list.append((pytz.utc.localize(datetime.datetime.utcnow()), amount))
 
     def withdraw(self, amount):
         if 0 < amount <= self.balance:
@@ -18,9 +24,17 @@ class Account:
             print("Sorry. You are broke. Not enough money on the bank")
             self.show_balance()
 
-
     def show_balance(self):
         print("Balance is {}".format(self.balance))
+
+    def show_transactions(self):
+        for date, amount in self.transaction_list:
+            if amount > 0:
+                transaction_type = "Deposited"
+            else:
+                transaction_type = "Withdrawn"
+                amount *= -1
+            print("{:6} {} on {} (local time was {})".format(amount, transaction_type, date, date.astimezone()))
 
 
 if __name__ == '__main__':
@@ -29,6 +43,9 @@ if __name__ == '__main__':
 
     Emilio.deposit(1000000)
     Emilio.deposit(1000000)
-    Emilio.withdraw(4000000)
+    Emilio.withdraw(500000)
+
+    Emilio.show_transactions()
+
 
 
